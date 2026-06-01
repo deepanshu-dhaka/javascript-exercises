3f7771d0e7c81270d581854559e4535d7a74dc97
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-01 03:50:31 +0000

    exercise 11_tempCoversion done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let decimalReuslt = (temp - 32) / 1.8;
+  let roundedResult = Math.round(decimalReuslt * 10) / 10;
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let decimalReuslt = (temp * 1.8) + 32;
+  let roundedResult = Math.round(decimalReuslt * 10) / 10;
+  return roundedResult;
 };
 
 // Do not edit below this line
