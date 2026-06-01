09afddf6ed63d3ad4ddc91c9cc37e758eed6c828
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-01 02:32:43 +0000

    exercise 08_removeFromArray done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...num) {
+    new_arr = arr.filter((item) => !num.includes(item))
+    return new_arr;
 };
 
 // Do not edit below this line
