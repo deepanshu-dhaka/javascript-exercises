56238851052b26dd704a966de355c05442e05278
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-01 01:58:00 +0000

    excercise 07_reverseString done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        result = result + str[i];
+    }
+    return result;
 
 };
 
