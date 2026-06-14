60144482dd7ee223f1e48482dce60e73160bc444
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-14 17:16:57 +0000

    Complete 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num === 0 || num === 1) {
+        return num;
+    }
+    else if (num < 0) {
+        return "OOPS"
+    }
+    else if (typeof num === "string") {
+        String(num);
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 0; i < num; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
 
+    }
+    return a;
 };
 
 // Do not edit below this line
