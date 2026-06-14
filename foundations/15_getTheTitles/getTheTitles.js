fa446f08566fe039398024c92a6fca715dc7c4d3
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-14 17:29:26 +0000

    Finish exercise 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let newBooks = books.map(function (item) {
+        return item.title;
+    })
+    return newBooks;
 };
 
 // Do not edit below this line
