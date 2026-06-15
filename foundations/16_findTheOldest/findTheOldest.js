f04bfff38e1f44ab7b165016bcd661e58bfb1ddb
deepanshu-dhaka <dhakadeepanshu30@gmail.com>
2026-06-15 10:37:04 +0000

    Complete exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let max = 0;
+    return people.reduce(function (acc, curr) {
+        if (!curr.yearOfDeath) {
+            const d = new Date();
+            yearOfDeath = d.getFullYear();
+            age = yearOfDeath - curr.yearOfBirth;
 
+        } else age = curr.yearOfDeath - curr.yearOfBirth;
+        if (max < age) {
+            max = age;
+            acc = curr;
+        }
+        return acc;
+    }, 0)
 };
 
 // Do not edit below this line
